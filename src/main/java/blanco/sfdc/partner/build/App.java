f10afc1760464337166f7f2be4d18282bdd65112
Toshiki Iga <igapyon@gmail.com>
2017-08-26 11:09:42 +0900

    TODOを追記。

--- src/main/java/blanco/sfdc/partner/build/App.java
+++ src/main/java/blanco/sfdc/partner/build/App.java
@@ -1,13 +1,15 @@
 package blanco.sfdc.partner.build;
 
 /**
- * Hello world!
- *
+ * Simple project to build sfdc-partner.jar file from partner.wsdl.
+ * 
+ * mvn archetype:generate -DgroupId=blanco.sfdc.partner.build
+ * -DartifactId=blanco-sfdc-partner-build
+ * -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
